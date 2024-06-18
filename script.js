7cbb89d19c23a24de52323d5cb8a95280c5428a8
Olga Skrypets <114450143+uhpoler@users.noreply.github.com>
2024-06-18 17:33:38 +0300

    Add displayMovements

--- script.js
+++ script.js
@@ -56,3 +56,18 @@ const inputTransferAmount = document.querySelector('.form__input--amount');
 const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
+
+const displayMovements = function (movements) {
+  movements.forEach(function (mov, i) {
+    //containerMovements.innerHTML = '';
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+    const html = `
+    <div class="movements__row">
+      <div class="movements__type 
+      movements__type--${type}">${i + 1} ${type}</div>
+      <div class="movements__value"> ${mov} €</div>
+    </div>`;
+
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
