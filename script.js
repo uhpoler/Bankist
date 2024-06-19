49f0a22ca81ac0c5617b875f23b5f3af4f859ee5
Olga Skrypets <114450143+uhpoler@users.noreply.github.com>
2024-06-19 12:40:15 +0300

    Add loan amount

--- script.js
+++ script.js
@@ -158,6 +158,21 @@ btnLogin.addEventListener('click', function (e) {
   }
 });
 
+btnLoan.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const amount = Number(inputLoanAmount.value);
+
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+    // Add movement
+    currentAccount.movements.push(amount);
+
+    // Update UI
+    updateUI(currentAccount);
+  }
+  inputLoanAmount.value = '';
+});
+
 btnTransfer.addEventListener('click', function (e) {
   e.preventDefault();
   const amount = Number(inputTransferAmount.value);
